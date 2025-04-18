8780a43c28aa4823dc4d343f7e6e836d49bb8db4
avinna <cloud.nayak3@gmail.com>
2025-04-18 09:14:01 +0530

    update code

---
+++ sonar-project.js
@@ -0,0 +1,17 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://13.235.19.152:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsMSS',
+	    'sonar.login': 'squ_3898cfbc0d999be7ad06a9495e08ca0890007cf8',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
